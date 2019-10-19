516a4ca49dba7173ccbe89030408aea1038eb316
Filza Valorisantyo <filza@Filzas-MacBook-Pro.local>
2019-10-19 12:23:51 +0700

    Update README and loginAutomation

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# TestPrixa
+# QA Test Prixa
+Kode ini dibuat untuk menyelesaikan automation dari https://platform.kata.ai/login dan https://user.kata.ai/signup/
+
+## Getting Started
+Terdapat dua buah kode yaitu loginAutomation.py untuk halaman login serta registerAutomation.py untuk halaman register
+
+### Prerequisites
+Pada bagian ini akan dijelaskan apa saja yang perlu diinstall untuk menjalankan kode ini
+
+```
+Python ver 2.7.16
+Selenium 3.141.0
+Chrome Webdriver
+```
+
+### Installing
+Python
+
+```
+ketik pip install -r requirements.txt pada virtual environment anda
+kemudian pastikan Chrome Webdriver yang telah didownload berada pada direktori yang sama dengan envirenment path
+```
+
+## Running the tests
+Python 
+
+```
+untuk login jalankan python loginAutomation.py
+untuk register jalankan python registerAutomation.py
+```
+
+## Built with
+* [Selenium](https://www.seleniumhq.org/)
+* [Python](https://www.python.org/)
+
+## Authors
+
+* **Filza Valorisantyo** - [filzav](https://github.com/filzav)
--- loginAutomation.py
+++ loginAutomation.py
@@ -7,10 +7,17 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait as wait
 from selenium.webdriver.support import expected_conditions as EC
 
-url = "https://platform.kata.ai/login"
+url = 'https://platform.kata.ai/login'
 xpath_username = "//input[@placeholder='Username']"
 xpath_password = "//input[@placeholder='Password']"
 xpath_submit = "//button[@class='sc-kpOJdX hniTIz']"
+xpath_remember = '//*[@id="remember-me"]'
+xpath_forgot = "//a[contains(text(),'Forgot password?')]"
+xpath_register = "//a[@class='text-bold']"
+xpath_homepage = "//a[contains(text(),'Kata.ai')]"
+xpath_errorLogin = "//div[@class='sc-dVhcbM diJyZz']"
+xpath_tutorial = "//a[contains(text(),'See Tutorial')]"
+xpath_docs = "//a[contains(text(),'View Docs')]"
 
 class loginAutomation(unittest.TestCase):
     def setUp(self):
@@ -25,10 +32,99 @@ class loginAutomation(unittest.TestCase):
 
         #2 Enter username, password
         username = driver.find_element_by_xpath(xpath_username)
-        username.sendKeys('')
+        username.send_keys('hakermusic')
         password = driver.find_element_by_xpath(xpath_password)
-        password.sendKeys('')
+        password.send_keys('buattestprixa')
         driver.find_element_by_xpath(xpath_submit).click()
+
+    def test_login_success_with_remember_me(self):
+        driver = self.driver
+        #1 Visit https://platform.kata.ai/login
+        driver.get(url)
+        self.assertIn('Kata Platform', self.driver.title)
+
+        #2 Enter username, password, click remember me
+        username = driver.find_element_by_xpath(xpath_username)
+        username.send_keys('hakermusic')
+        password = driver.find_element_by_xpath(xpath_password)
+        password.send_keys('buattestprixa')
+        driver.find_element_by_xpath(xpath_remember).click()
+        driver.find_element_by_xpath(xpath_submit).click()
+
+    def test_go_to_forgot_password_page(self):
+        driver = self.driver
+        #1 Visit https://platform.kata.ai/login
+        driver.get(url)
+        self.assertIn('Kata Platform', self.driver.title)
+
+        #2 Go to forgot password page
+        driver.find_element_by_xpath(xpath_forgot).click()
+
+    def test_go_to_register_page(self):
+        driver = self.driver
+        #1 Visit https://platform.kata.ai/login
+        driver.get(url)
+        self.assertIn('Kata Platform', self.driver.title)
+
+        #2 Go to register page
+        wait(driver, 50).until(EC.element_to_be_clickable((By.XPATH,xpath_register))).click()
+
+    def test_go_to_homepage(self):
+        driver = self.driver
+        #1 Visit https://platform.kata.ai/login
+        driver.get(url)
+        self.assertIn('Kata Platform', self.driver.title)
+
+        #2 Go to Kata.ai homepage
+        wait(driver, 50).until(EC.element_to_be_clickable((By.XPATH,xpath_homepage))).click()
+
+    def test_login_failed_wrong_username(self):
+        driver = self.driver
+        #1 Visit https://platform.kata.ai/login
+        driver.get(url)
+        self.assertIn('Kata Platform', self.driver.title)
+
+        #2 Enter wrong username and password
+        username = driver.find_element_by_xpath(xpath_username)
+        username.send_keys('hakermusic')
+        password = driver.find_element_by_xpath(xpath_password)
+        password.send_keys('aaaaaaaaaaaa')
+        driver.find_element_by_xpath(xpath_submit).click()
+        wait(driver, 10).until(EC.text_to_be_present_in_element((By.XPATH,xpath_errorLogin), 'Invalid username or password'))
+
+
+    def test_login_failed_wrong_username_and_password(self):
+        driver = self.driver
+        #1 Visit https://platform.kata.ai/login
+        driver.get(url)
+        self.assertIn('Kata Platform', self.driver.title)
+
+        #2 Enter wrong username and password
+        username = driver.find_element_by_xpath(xpath_username)
+        username.send_keys('aaaaaaaaaaaa')
+        password = driver.find_element_by_xpath(xpath_password)
+        password.send_keys('aaaaaaaaaaaa')
+        driver.find_element_by_xpath(xpath_submit).click()
+        wait(driver, 10).until(EC.text_to_be_present_in_element((By.XPATH,xpath_errorLogin), 'Invalid username or password'))
+
+    def test_go_to_tutorial_page(self):
+        driver = self.driver
+        #1 Visit https://platform.kata.ai/login
+        driver.get(url)
+        self.assertIn('Kata Platform', self.driver.title)
+
+        #2 Click See Tutorial button
+        wait(driver, 10).until(EC.element_to_be_clickable((By.XPATH,xpath_tutorial))).click()
+
+    def test_go_to_docs_page(self):
+        driver = self.driver
+        #1 Visit https://platform.kata.ai/login
+        driver.get(url)
+        self.assertIn('Kata Platform', self.driver.title)
+
+        #2 Click See Tutorial button
+        wait(driver, 10).until(EC.element_to_be_clickable((By.XPATH,xpath_docs))).click()
+
 
     def tearDown(self):
         time.sleep(5)
